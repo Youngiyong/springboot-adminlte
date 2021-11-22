a39288091a097a6781739bb4413cca5326f693f2
Gi-Dragon <youn9354@naver.com>
2021-11-23 03:15:04 +0900

    spring security fix

--- src/main/java/com/hendisantika/adminlte/security/WebSecurityConfig.java
+++ src/main/java/com/hendisantika/adminlte/security/WebSecurityConfig.java
@@ -25,8 +25,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers("/bootstrap/**", "/dist/**", "/plugins/**").permitAll()
                 .antMatchers("/login").permitAll() // 누구나 접근 허용
-                .antMatchers("/").hasRole("MEMBER") // USER, ADMIN만 접근 가능
-                .antMatchers("/").hasRole("ADMIN") // USER, ADMIN만 접근 가능
+                .antMatchers("/").hasAnyRole("MEMBER", "ADMIN") // MEMBER, ADMIN만 접근 가능
                 .antMatchers("/users").hasRole("ADMIN") // ADMIN만 접근 가능
                 .anyRequest().authenticated() // 나머지 요청들은 권한의 종류에 상관 없이 권한이 있어야 접근 가능
                 .and()
